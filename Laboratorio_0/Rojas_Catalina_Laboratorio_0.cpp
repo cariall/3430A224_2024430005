5171a973163dc1f167486bcc1e5ecb54c0bb3bf6
cariall <catrsep@hotmail.com>
2025-08-25 15:03:12 -0400

    Función para determinar número par o impar

---
+++ Laboratorio_0/Rojas_Catalina_Laboratorio_0.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int par_o_no(int numero) {
+	if (numero%2 == 1) {
+		std::cout<<"El número " << numero << " es impar\n";
+	} else {
+		std::cout<<"El número " << numero << " es par\n";
+	}
+	return numero;
+}
+
+int main() {
+	int numero;
+	std::cout<<"Ingrese un número: \n";
+	std::cin>>numero;
+	par_o_no(numero);
+	std::cout<<"El número que ingresaste es "<<numero<<"\n";
+	return 0;
+}
+
