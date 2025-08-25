c1367f1a0dc678f687a25958355339853bec7a0c
cariall <catrsep@hotmail.com>
2025-08-24 23:55:07 -0400

    Update

--- Laboratorio_0/Rojas_Catalina_CalculoCuadrado.cpp
+++ Laboratorio_0/Rojas_Catalina_CalculoCuadrado.cpp
@@ -1,1 +1,16 @@
-#include <iostream> 
+#include <iostream>
+double square(double x) //double: 64 bits, más precisión que float (32)
+{
+return x*x;
+}
+
+void print_square(double x)
+{
+std::cout<<"El cuadrado de " << x << " es " << square(x) << "\n";
+}
+
+int main()
+{
+print_square(1.234);
+print_square(5.555);
+} 
